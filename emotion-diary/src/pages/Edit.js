1a4b37aefad490e8b904a313a8f61705ee505488
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-20 03:26:46 +0900

    Add : Route 추가

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  const id = searchParams.get('id');
+  console.log("id : ", id);
+
   return (
   <div>
     <h1>Edit</h1>
     <p>이것은 일기 수정 페이지 입니다.</p>
+    <button onClick={()=>setSearchParams({who:'maeng'})}>
+      QS바꾸기
+    </button>
+    <button onClick={()=>{navigate('/home')}}>
+      Home으로 가기
+    </button>
+    <button onClick={()=> {navigate(-1);}}>
+      뒤로가기
+    </button>
   </div>
   );
 };

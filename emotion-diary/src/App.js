45ae459072bd60f2993a4c0b08b2ca308e243dc3
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-20 03:06:41 +0900

    Setting : 환경 세팅

---
+++ emotion-diary/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import RouteTest from './components/RouteTest';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <h2>하이</h2>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/new" element={<New/>}/>
+        <Route path="/edit" element={<Edit/>}/>
+        <Route path="/diary" element={<Diary/>}/>
+      </Routes>
+      <RouteTest/>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

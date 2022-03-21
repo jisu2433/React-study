0f293f9799dc27f2d5e79d8156a85cfb9a886396
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-22 04:31:08 +0900

    Add : Home 페이지 구현

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -35,9 +35,44 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id:1,
+    emotion:1,
+    content:"오늘의 일기 1번",
+    date : 1647853293053,
+  },
+  {
+    id:2,
+    emotion:2,
+    content:"오늘의 일기 2번",
+    date : 1647853293054,
+  },
+  {
+    id:3,
+    emotion:3,
+    content:"오늘의 일기 3번",
+    date : 1647853293055,
+  },
+  {
+    id:4,
+    emotion:4,
+    content:"오늘의 일기 4번",
+    date : 1647853293056,
+  },
+  {
+    id:5,
+    emotion:5,
+    content:"오늘의 일기 5번",
+    date : 1647853293057,
+  }
+];
+
 function App() {
 
-  const [data, dispatch] = useReducer(reducer,[])
+  const [data, dispatch] = useReducer(reducer,dummyData);
+
+  console.log(new Date().getTime());
 
   const dataId = useRef(0);
   // CREATE

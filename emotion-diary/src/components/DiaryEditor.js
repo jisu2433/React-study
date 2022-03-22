c5bebbad27c9d5dff0c83c943cb33a2ff1ac8f90
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-22 23:16:58 +0900

    Add : 상세 페이지

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -6,40 +6,8 @@ import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import EmotionItem from "./EmotionItem";
 
-const env = process.env;
-env.PUBLIC_URL = env.PUBLIC_URL || "";
-
-const emotionList = [
-  {
-    emotion_id : 1,
-    emotion_img : process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: '완전 좋음'
-  },
-  {
-    emotion_id : 2,
-    emotion_img : process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: '좋음'
-  },
-  {
-    emotion_id : 3,
-    emotion_img : process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: '보통'
-  },
-  {
-    emotion_id : 4,
-    emotion_img : process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: '나쁨'
-  },
-  {
-    emotion_id : 5,
-    emotion_img : process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: '완전 나쁨'
-  }
-]
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const DiaryEditor = ({isEdit, originData}) => {
   const [emotion, setEmotion] = useState(3);

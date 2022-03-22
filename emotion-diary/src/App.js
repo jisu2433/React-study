4534c0496ea166af539b5b37edc4f2b3692cb00b
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-23 04:36:02 +0900

    Add : title 추가

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -41,12 +41,19 @@ function App() {
 
   useEffect(()=>{
     const localData = localStorage.getItem('diary');
+
     if(localData){
-      const diaryList = JSON.parse(localData).sort((a,b)=> parseInt(b.id) - parseInt(a.id));
-      dataId.current = parseInt(diaryList[0].id) + 1;
-      dispatch({type:"INIT", data:diaryList});
+      const diaryList = JSON.parse(localData).sort(
+        (a,b)=> parseInt(b.id) - parseInt(a.id)
+      );
+
+      if (diaryList.length >= 1){
+        dataId.current = parseInt(diaryList[0].id) + 1;
+        dispatch({type:"INIT", data:diaryList});
+      }
     }
-  },[])
+  }, []);
+
   const dataId = useRef(6);
   // CREATE
   const onCreate = (date, content, emotion) => {
--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -10,6 +10,11 @@ const Home = () => {
   const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`
   
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `독서 기록장`;
+  },[]);
+
   useEffect(()=>{
     if (diaryList.length >= 1){
     const firstDay = new Date(
